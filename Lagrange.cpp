b311867fd5be388d1beb1d4b83f9e803c4d58d5e
ilyas <abdylovilas@gmail.com>
2022-11-04 15:31:16 +0500

    second commit

--- Lagrange.cpp
+++ Lagrange.cpp
@@ -5,13 +5,13 @@
 #include<stdio.h>
 #include<cmath>
 
-double expression(double x) {
+double expression(double x) { //функция из варианта
 	return exp(x) / (1 + x * x);
-}
+} 
 
 
 
-void Josepf_Lui_Lagrange_is_thinking(double (*function)(double),std::vector<double> &X,std::vector<double> &Coeffisients){
+void Lagrange_coeffs(double (*function)(double),std::vector<double> &X,std::vector<double> &Coeffisients){ //получает функцию, сетку, пустой вектор; заполняет вектор коэффициентами многочлена Лагранжа для данной функции по этой сетке
 		double a = 1;
 		int n = X.size();
 		for(int i = 0;i<n;i++){
@@ -27,8 +27,8 @@ void Josepf_Lui_Lagrange_is_thinking(double (*function)(double),std::vector<doub
 
 
 
-double Josepf_Lui_Lagrange_is_calculating(double parametr_x,std::vector<double> &X,std::vector<double> &Coeffisients){
-	int n = Coeffisients.size();
+double Lagrange_value(double parametr_x,std::vector<double> &X,std::vector<double> &Coeffisients){
+	int n = Coeffisients.size(); //вычисление значения многочлена Лагранжа в заданной точке
 	double result = 0;
 	for(int i = 0;i<n;i++){
 		double product_i = 1;
@@ -41,8 +41,8 @@ double Josepf_Lui_Lagrange_is_calculating(double parametr_x,std::vector<double> 
 	return result;
 }
 
-double Josepf_Lui_Lagrange_give_me_order(double (*function)(double),double a,double b,int n,double x){
-	//double a = 0,b = 2;
+double Lagrange(double (*function)(double),double a,double b,int n,double x){
+	//double a = 0,b = 2; //строит на отрезке [a;b] равномерную n-сетку, строит на этой сетке для заданной функции лагранжеву интерполяцию (Lagrange_coeffs), затем вычисляет значение этой интерполяции в заданной точке (Lagrange_value); возвращает полученное значение
 	//int n = 10;
 	double h = (b-a)/(n);
 	n++;
@@ -52,31 +52,28 @@ double Josepf_Lui_Lagrange_give_me_order(double (*function)(double),double a,dou
 	for(int i = 0;i<n;i++){
 		X.push_back(a+h*i);
 	}
-	Josepf_Lui_Lagrange_is_thinking(function,X,Coeffisients);
-	double res = Josepf_Lui_Lagrange_is_calculating(x,X,Coeffisients);
+	Lagrange_coeffs(function,X,Coeffisients);
+	double res = Lagrange_value(x,X,Coeffisients);
 	
 	//std::cout<<"Josepf Lui Lagrange is tired. But he could complete the calculation and found number is "<<res<<std::endl;
 	
 	return res;
 }
 
-int write_n;
-
-double Josepf_Lui_Lagrange_max_difference(double (*function)(double),double a,double b,int n){
+double Lagrange_max_deviation(double (*function)(double),double a,double b,int n){ //вычисляет наибольшее отклонение многочлена L_n (x) от начальной функции, перебирая значения на равномерной 10^5-сетке
 
 	
-	int delta_n = 100000;
-	double h = (b-a)/delta_n;
-	delta_n++;
+	int grid = 100000;
+	double h = (b-a)/grid;
+	grid++;
 	std::vector<double> X;
-	for(int i = 0;i<delta_n;i++) X.push_back(a+i*h);
+	for(int i = 0;i<grid;i++) X.push_back(a+i*h);
 	
 	double max = 0;
 	double value;
 	//printf("exp(x) = %lf\n",expression());
-	for(int i = 0;i<delta_n;i++){
-		//printf("expr(x) = %lf and interpolation(x) = %lf\n",function(X[i]),Josepf_Lui_Lagrange_give_me_order(function,a,b,n,X[i]));
-		value = function(X[i]) - Josepf_Lui_Lagrange_give_me_order(function,a,b,n,X[i]);
+	for(int i = 0;i<grid;i++){
+		value = function(X[i]) - Lagrange(function,a,b,n,X[i]);
 		value = std::abs(value);
 	
 		if(value>max) max = value;
@@ -85,58 +82,35 @@ double Josepf_Lui_Lagrange_max_difference(double (*function)(double),double a,do
 	return max;
 }
 
-
-
-
-
-int task1(){
+int task2(int n_max){ //для всех n от 1 до n_max вычисляет максимальное отклонение (Lagrange_max_deviation) многочлена Лагранжа от заданной функции; возвращает n_0; строит график отклонения f от n_0
 	FILE* out = fopen("Lagrange/max_difference.txt","w");
-	double max_difference[15];
-	int min_i = 1;
+	double max_difference[n_max];
+	int n_0 = 1;
 	//max_difference[0] = 0;
 	
 	
-	for(int n = 1;n<16;n++){
-		max_difference[n-1] = Josepf_Lui_Lagrange_max_difference(expression,0,2,n);
+	for(int n = 1;n<=n_max;n++){
+		max_difference[n-1] = Lagrange_max_deviation(expression,0,2,n);
 		printf("n = %d\t max_dif = %lf\n",n,max_difference[n-1]);
 		fprintf(out,"%d %lf\n",n,max_difference[n-1]);
-		if(max_difference[n-1]<max_difference[min_i]) min_i = n-1;
+		if(max_difference[n-1]<max_difference[n_0]) n_0 = n-1;
 	}
-	printf("optimal n is %d\n",min_i);
-	return 0;
-}
+	printf("optimal n is %d\n",n_0);
 
-
-double Josepf_Lui_Lagrange_show(double (*function)(double),double a,double b,int n){
-	FILE* out;
-	//if(write_n==n)
-	out = fopen("Lagrange/Lagrange.txt","w");
-	
-	
-	int delta_n = 1000;
-	double h = (b-a)/delta_n;
-	delta_n++;
+	FILE* dev_0 = fopen("Lagrange/n0_difference.txt", "w");
+	int grid = 100000;
+	double h = (b - a) / grid;
+	grid++;
 	std::vector<double> X;
-	for(int i = 0;i<delta_n;i++) X.push_back(a+i*h);
-	
-	double max = 0;
-	double value;
-	//printf("exp(x) = %lf\n",expression());
-	for(int i = 0;i<delta_n;i++){
-		//printf("expr(x) = %lf and interpolation(x) = %lf\n",function(X[i]),Josepf_Lui_Lagrange_give_me_order(function,a,b,n,X[i]));
-		value = Josepf_Lui_Lagrange_give_me_order(function,a,b,n,X[i]);
-	
-		fprintf(out,"%lf %lf\n",X[i],value);
-	
-		//if(value>max) max = value;
+	for (int i = 0; i < grid; i++) X.push_back(a + i * h);
+	for (int i = 0; i < grid; i++) {
+		fprintf(dev_0, "%d %lf\n", x[i], function(X[i]) - Lagrange(function, a, b, n, X[i]);
 	}
-	
-	return max;
+	return 0;
 }
 
 int main(){
 	int n = 10;
-	Josepf_Lui_Lagrange_show(expression,0,2,n);
-	task1();
+	task2();
 	return 0;
 }
